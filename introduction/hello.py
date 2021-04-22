4224abec1f34e2fdddcb0ed5d85a8eafb6a292ce
Ashwin0512 <f20202163@hyderbad.bits-pilani.ac.in>
2021-04-23 02:58:47 +0530

    hello world

--- introduction/hello.py
+++ introduction/hello.py
@@ -3,4 +3,15 @@ print("Hello World !!")
 # SINGLE LINE Comments
 ''' MULTILINE 
 Comments '''
+
+print()
+
+print('''Mera
+naam
+Ashwin              
+hai''')
+
+#this is how we print multiline strings in python. 
+
+
  

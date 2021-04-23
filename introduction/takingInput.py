9aa3c9b41faf6c627c61c0a6e3dcb66f7fcf772d
Ashwin0512 <f20202163@hyderbad.bits-pilani.ac.in>
2021-04-23 15:53:42 +0530

    taking user input in python

---
+++ introduction/takingInput.py
@@ -0,0 +1,9 @@
+a = input("Enter your name : ")
+print("Your name is : ",a)
+
+b = input("Enter first number : ")
+c = input("Enter second number : ")
+b = int(b)
+c = int(c)
+
+print("Sum of ",a," and ",b," is ",b+c)

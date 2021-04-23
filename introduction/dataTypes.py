b48f097ab13b5336623524b9d3024bbd264abcb6
Ashwin0512 <f20202163@hyderbad.bits-pilani.ac.in>
2021-04-23 12:06:21 +0530

    data types in python

---
+++ introduction/dataTypes.py
@@ -0,0 +1,19 @@
+a = 123
+b = 12.35
+c = "Ashwin"
+d = True
+e = None
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+
+print()
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
